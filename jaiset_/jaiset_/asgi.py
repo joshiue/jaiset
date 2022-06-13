1e95f3d68a728acbd97f26432140cf19fcd3e65e
joshiue <jaitonit@yahoo.co.uk>
2022-06-13 13:10:09 +0100

    python virtual env with django installed

---
+++ jaiset_/jaiset_/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for jaiset_ project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jaiset_.settings')
+
+application = get_asgi_application()
